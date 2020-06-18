27e967cd05d64c077ab02c9e492b36a5a769755a
Endramar <alikskyl@gmail.com>
2020-06-18 22:45:13 +0300

    auth check state and localstorage added

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,6 +25,9 @@ export const authFail = (error) => {
 }
 
 export const authLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationDate");
+    localStorage.removeItem("userId");
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -55,6 +58,13 @@ export const auth = (email, password, isSignUp) => {
 
         axios.post(url, authData).then(response => {
             console.log(response);
+
+            const exprirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+
+            localStorage.setItem("token", response.data.idToken);
+            localStorage.setItem("exprirationDate", exprirationDate);
+            localStorage.setItem("userId", response.data.localId);
+
             dispatch(authSuccess(response.data.idToken, response.data.localId));
             dispatch(checkAuthTimeout(response.data.expiresIn));
         }).catch(error => {
@@ -67,7 +77,28 @@ export const auth = (email, password, isSignUp) => {
 
 export const setAuthRedirectPath = (path) => {
     return {
-        type : actionTypes.SET_AUTH_REDIRECT,
-        path : path
+        type: actionTypes.SET_AUTH_REDIRECT,
+        path: path
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem("token");
+
+        if (!token) {
+            dispatch(authLogout());
+        }
+        else {
+            const expirationDate = new Date(localStorage.getItem("expirationDate"));
+            if (expirationDate > new Date()) {
+                const userId = localStorage.getItem("userId");
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+            }
+            else {
+                dispatch(authLogout());
+            }
+        }
     }
 }
